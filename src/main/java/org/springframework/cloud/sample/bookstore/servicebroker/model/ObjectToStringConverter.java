68d2fdc7bd319b1234a53ed2277f9ca3b989fdd0
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-09-12 15:25:12 +0800

    处理map类型参数转义

--- src/main/java/org/springframework/cloud/sample/bookstore/servicebroker/model/ObjectToStringConverter.java
+++ src/main/java/org/springframework/cloud/sample/bookstore/servicebroker/model/ObjectToStringConverter.java
@@ -16,16 +16,31 @@
 
 package org.springframework.cloud.sample.bookstore.servicebroker.model;
 
+import net.sf.json.JSONObject;
+
 import javax.persistence.AttributeConverter;
+import java.util.Map;
 
 public class ObjectToStringConverter implements AttributeConverter<Object, String> {
 	@Override
 	public String convertToDatabaseColumn(Object attribute) {
+
+		JSONObject json = null;
+		if (attribute instanceof Map){
+			json = JSONObject.fromObject(attribute);
+			return json.toString();
+		}
 		return attribute.toString();
 	}
 
 	@Override
 	public Object convertToEntityAttribute(String dbData) {
+
+		if (dbData.startsWith("{")) {
+			JSONObject json = JSONObject.fromObject(dbData);
+			return json;
+		}
+
 		return dbData;
 	}
 }
